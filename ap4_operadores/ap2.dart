f51a965a4bd3a2df9630694736baa6726699bcb3
Guilherme Zaguini <guilherme.zaguini2018@gmail.com>
2026-05-28 15:59:48 -0300

    Envio da Atividade 2

--- ap4_operadores/ap2.dart
+++ ap4_operadores/ap2.dart
@@ -1,15 +1,23 @@
 void main() {
+// Lista de strings para converter
 final lista = ['10', '2XXL7', 'JOJ0', '99', '381', 'AD44', '47', '2B', '123', '78'];
 
-texto(lista);
+// Chamando a função para converter a lista de strings em números inteiros
+converterLista(lista);
 
 }
 
-final convertidos = [];
-void texto(List<String> lista){
+// Lista para armazenar os números convertidos
+final convertidos = <int>[];
+// Função para converter a lista de strings em números inteiros
+void converterLista(List<String> lista){
+  // Iterando sobre cada string na lista
   for(var texto in lista){
+    // Tentando converter a string para um número inteiro, se não for possível, retorna 0
    var numero = int.tryParse(texto) ?? 0;
+   // Adicionando o número convertido à lista de convertidos
    convertidos.add(numero);
   }
-   print(convertidos);
+  // Imprimindo a lista de números convertidos
+   print('Lista convertida: ${convertidos.join(', ')}');
 }
