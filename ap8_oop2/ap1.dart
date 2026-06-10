88029d204bc7a04fbee9ee65772b8f76dd8dd906
Guilherme Zaguini <guilherme.zaguini2018@gmail.com>
2026-06-09 21:04:41 -0300

    Envio da Atividade 1 Gêneros musicais

---
+++ ap8_oop2/ap1.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+void main ( ){
+final numeroAleatorio = Random().nextInt(GenerosMusicais.values.length);
+  print(numeroAleatorio);
+
+  // Pegar um valor do enum com base no número gerado Aleatorio
+  final generoFavorito = GenerosMusicais.values[numeroAleatorio];
+  switch (generoFavorito) {
+    case GenerosMusicais.trap:
+      print('Meu gênero musical preferido e o trap');
+      break;
+    case GenerosMusicais.pagode:
+      print('Meu gênero musical preferido e o pagode');
+      break;
+    case GenerosMusicais.samba:
+      print('Meu gênero musical preferido e o samba');
+      break;
+    case GenerosMusicais.rock:
+      print('Meu gênero musical preferido e o rock');
+      break;
+    case GenerosMusicais.pop:
+      print('Meu gênero musical preferido e o pop');
+      break;
+    case GenerosMusicais.rap:
+      print('Meu gênero musical preferido e o rap');
+      break;
+  }
+
+}
+
+// Defeni os Gêneros musicais
+enum GenerosMusicais {
+  trap,
+  pagode,
+  samba,
+  rock,
+  pop,
+  rap,
+}
+
+
