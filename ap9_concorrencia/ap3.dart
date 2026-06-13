1514f5027a14beb1214593a00895ebf5bfb209b8
Guilherme Zaguini <guilherme.zaguini2018@gmail.com>
2026-06-13 11:20:03 -0300

    Envio da Atividade 3 Comentada (Correção)

--- ap9_concorrencia/ap3.dart
+++ ap9_concorrencia/ap3.dart
@@ -1,20 +1,35 @@
+// Função principal do programa
 void main() async {
+
+  // Exibe a mensagem de início da contagem regressiva
   print('Iniciando lançamento');
 
+  // Variável que controla a contagem regressiva
   var contador = 5;
 
-  while( contador > 0 ){
-    await contagem(contador);
-    contador-- ;
-  
-  }
-    
+  // Executa enquanto o contador for maior que zero
+  while (contador > 0) {
 
+    // Aguarda a exibição do número atual da contagem
+    await contagem(contador);
+
+    // Decrementa o contador em 1
+    contador--;
+
+  }
+
+  // Exibe a mensagem de lançamento após o término da contagem
   print('Foguete lançado!');
+
 }
 
+// Função assíncrona responsável pela contagem regressiva
 Future<void> contagem(int value) async {
-    print('$value');
-   await Future.delayed(Duration(seconds: 1));
+
+  // Exibe o valor atual da contagem
+  print('$value');
+
+  // Aguarda 1 segundo antes de continuar
+  await Future.delayed(Duration(seconds: 1));
 
 }
